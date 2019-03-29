205be62a08654ae59c8bed2613f24e89bd26abd6
Adetola <adebolaadetola65@gmail.com>
2019-03-29 01:23:49 +0100

    refactor Message.java

--- app/src/main/java/com/deytola/assignment/Message.java
+++ app/src/main/java/com/deytola/assignment/Message.java
@@ -2,19 +2,20 @@ package com.deytola.assignment;
 
 public class Message {
     private String text;  //message to be displayed
-    private boolean isPublisher; //is message published by user?
+    private boolean isPublisher; //is message sent by publisher or subscriber?
 
 
-    public Message (String text, boolean isPublisher){
+    public Message(String text, boolean isPublisher) {
         this.text = text;
         this.isPublisher = isPublisher;
     }
 
-    public String getText(){
+    public String getText() {
         return text;
     }
 
-    public boolean isUserPublisher(){
+    public boolean isUserPublisher() {
+
         return isPublisher;
     }
 }
